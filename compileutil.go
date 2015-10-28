f60f2f21ba79fad8938076ebd538e0c8f17b874d
Theodus <theodusbutler@gmail.com>
2015-10-28 15:23:47 +0000

    need to test cli in environment containing haxe

--- compileutil.go
+++ compileutil.go
@@ -1,59 +1,62 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
 	"os"
 	"os/exec"
+	"fmt"
+	"bufio"
+	"github.com/codegangsta/cli"
 )
 
 func main() {
-	args := os.Args[1:]
-	if len(args) < 2 {
-		fmt.Println("Please specify the package and target language. (java, cpp, cs)")
-		return
-	} else if len(args) > 2 {
-		fmt.Println("Too many arguments!")
-		return
+	app := cli.NewApp()
+	app.Name = "tardisgo-compileutil"
+	app.Usage = "compile Go to Java, C++, and C# using tardisgo"
+	app.Commands = []cli.Command{
+		{
+			Name:   "java",
+			Usage:  "Compile Go source to Java target",
+			Action: func(ctx *cli.Context) {
+				cmd("./bin/tardisgo", ctx.Args()[0])
+				cmd("haxe", "-main", "tardis.Go", "-cp", "tardis", "-dce", "full", "-java", "tardis/java")
+				_, err := os.Stat("bin/java")
+				if os.IsNotExist(err) {
+					cmd("mkdir", "bin/java")
+				}
+				cmd("cp", "tardis/java/Go.jar", "bin/java/")
+			},
+		}, {
+			Name:   "cpp",
+			Usage:  "Compile Go source to C++ target",
+			Action: func(ctx *cli.Context) {
+				cmd("./bin/tardisgo", ctx.Args()[0])
+				cmd("haxe", "-main", "tardis.Go", "-cp", "tardis", "-dce", "full", "-cpp", "tardis/cpp")
+				cmd("mkdir", "bin/cpp")
+				_, err := os.Stat("bin/cpp")
+				if os.IsNotExist(err) {
+					cmd("mkdir", "bin/cpp")
+				}
+				cmd("cp", "tardis/cpp/Go", "bin/cpp/")
+			},
+		}, {
+			Name:   "cs",
+			Usage:  "Compile Go source to C# target",
+			Action: func(ctx *cli.Context) {
+				cmd("./bin/tardisgo", ctx.Args()[0])
+				cmd("haxe", "-main", "tardis.Go", "-cp", "tardis", "-dce", "full", "-cs", "tardis/cs")
+				cmd("mkdir", "bin/cs")
+				_, err := os.Stat("bin/cs")
+				if os.IsNotExist(err) {
+					cmd("mkdir", "bin/cs")
+				}
+				cmd("cp", "tardis/cs/Go", "bin/cs/")
+			},
+		},
 	}
-	_, err := os.Stat("src/github.com/tardisgo/tardisgo")
-	if os.IsNotExist(err) {
-		cmd("go", []string{"get", "-u", "github.com/tardisgo/tardisgo"})
-	}
-
-	compArgs := []string{"-main", "tardis.Go", "-cp", "tardis", "-dce", "full"}
-	lang := args[1]
-	switch lang {
-	case "java":
-		compArgs = append(compArgs, "-java", "tardis/java")
-		cmd("./bin/tardisgo", []string{args[0]})
-		cmd("haxe", compArgs)
-		_, err = os.Stat("bin/java")
-		if os.IsNotExist(err) {
-			cmd("mkdir", []string{"bin/java"})
-		}
-		cmd("cp", []string{"tardis/java/Go.jar", "bin/java/"})
-	case "cpp":
-		compArgs = append(compArgs, "-cpp", "tardis/cpp")
-		cmd("./bin/tardisgo", []string{args[0]})
-		cmd("haxe", compArgs)
-		cmd("mkdir", []string{"bin/cpp"})
-		_, err = os.Stat("bin/cpp")
-		if os.IsNotExist(err) {
-			cmd("mkdir", []string{"bin/cpp"})
-		}
-		cmd("cp", []string{"tardis/cpp/Go", "bin/cpp/"})
-	case "cs":
-		compArgs = append(compArgs, "-cs", "tardis/cs")
-		cmd("./bin/tardisgo", []string{args[0]})
-		cmd("haxe", compArgs)
-	default:
-		fmt.Println(lang, "is not a target language. (java, cpp, cs)")
-		return
-	}
+	app.Run(os.Args)
 }
 
-func cmd(cmdName string, cmdArgs []string) {
+func cmd(cmdName string, cmdArgs ...string) {
 	cmd := exec.Command(cmdName, cmdArgs...)
 	cmdReader, err := cmd.StdoutPipe()
 	if err != nil {
