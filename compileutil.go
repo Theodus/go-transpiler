23f89a2df5dc1c3a0c944632c1be788e8492e75c
Theodus <theodusbutler@gmail.com>
2015-10-31 22:32:31 -0400

    pipe cmd out and err to std

--- compileutil.go
+++ compileutil.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"os/exec"
@@ -9,13 +10,14 @@ import (
 )
 
 func main() {
-	out, err := cmd("go", "get", "-u", "-v", "github.com/tardisgo/tardisgo")
-	if err != nil {
-		fmt.Println(out, err)
+	if err := cmd("go", "get", "-u", "-v", "github.com/tardisgo/tardisgo"); err != nil {
+		fmt.Println(err)
+		return
 	}
 	app := cli.NewApp()
 	app.Name = "tardisgo-compileutil"
 	app.Usage = "compile Go to Java, C++, and C# using tardisgo"
+	app.Version = "0.1.0"
 	app.Commands = []cli.Command{
 		{
 			Name:  "java",
@@ -65,49 +67,73 @@ func main() {
 }
 
 func build(lang, pkg, suf string) {
-	out, err := cmd("tardisgo", pkg)
-	if err != nil {
-		fmt.Println(out, err)
+	if err := cmd("tardisgo", pkg); err != nil {
+		fmt.Println(err)
 		return
 	}
-	out, err = cmd("haxe", "-main", "tardis.Go", "-cp", "tardis", "-dce", "full", fmt.Sprintf("-%s", lang), fmt.Sprintf("tardis/%s", lang))
+	err := cmd("haxe", "-main", "tardis.Go", "-cp", "tardis", "-dce", "full", fmt.Sprintf("-%s", lang), fmt.Sprintf("tardis/%s", lang))
 	if err != nil {
-		fmt.Println(out, err)
+		fmt.Println(err)
 		return
 	}
 	binDir := fmt.Sprintf("%s/bin/%s", os.Getenv("GOPATH"), lang)
 	_, err = os.Stat(binDir)
 	if os.IsNotExist(err) {
-		out, err = cmd("mkdir", binDir)
+		err = cmd("mkdir", binDir)
 		if err != nil {
-			fmt.Println(out, err)
+			fmt.Println(err)
 			return
 		}
 	}
 	if lang == "java" {
-		out, err = cmd("cp", fmt.Sprintf("tardis/%s/Go.jar", lang), binDir)
+		err = cmd("cp", fmt.Sprintf("tardis/%s/Go.jar", lang), binDir)
 		if err != nil {
-			fmt.Println(out, err)
+			fmt.Println(err)
 			return
 		}
 	} else if lang == "cpp" {
-		out, err = cmd("cp", fmt.Sprintf("tardis/%s/Go", lang), binDir)
+		err = cmd("cp", fmt.Sprintf("tardis/%s/Go", lang), binDir)
 		if err != nil {
-			fmt.Println(out, err)
+			fmt.Println(err)
 			return
 		}
 	} else {
-		out, err = cmd("cp", fmt.Sprintf("tardis/%s/Go.csproj", lang), binDir)
+		err = cmd("cp", fmt.Sprintf("tardis/%s/Go.csproj", lang), binDir)
 		if err != nil {
-			fmt.Println(out, err)
+			fmt.Println(err)
 			return
 		}
 	}
 	fmt.Println("binary placed in", binDir)
 }
 
-func cmd(cmdName string, cmdArgs ...string) (out string, err error) {
-	b, err := exec.Command(cmdName, cmdArgs...).CombinedOutput()
-	out = string(b)
-	return out, err
+func cmd(cmdName string, cmdArgs ...string) error {
+	cmd := exec.Command(cmdName, cmdArgs...)
+	outReader, err := cmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+	outScanner := bufio.NewScanner(outReader)
+	go func() {
+		for outScanner.Scan() {
+			fmt.Println(outScanner.Text())
+		}
+	}()
+	errReader, err := cmd.StderrPipe()
+	if err != nil {
+		return err
+	}
+	errScanner := bufio.NewScanner(errReader)
+	go func() {
+		for errScanner.Scan() {
+			fmt.Println(errScanner.Text())
+		}
+	}()
+	if err = cmd.Start(); err != nil {
+		return err
+	}
+	if err = cmd.Wait(); err != nil {
+		return err
+	}
+	return nil
 }
