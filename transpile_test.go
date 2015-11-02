18f41373e1eb243a9b226cb314f73eb599807d70
Theodus <theodusbutler@gmail.com>
2015-11-02 15:59:51 +0000

    remove test for cs target

--- transpile_test.go
+++ transpile_test.go
@@ -15,7 +15,7 @@ func TestJava(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	binDir := fmt.Sprintf("%s/bin/java/test.jar", os.Getenv("GOPATH"))
+	binDir := fmt.Sprintf("%sbin/java/test.jar", os.Getenv("GOPATH"))
 	_, err = os.Stat(binDir)
 	if os.IsNotExist(err) {
 		t.Fatal("jar file not in ", binDir)
@@ -31,26 +31,10 @@ func TestCPP(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	binDir := fmt.Sprintf("%s/bin/cpp/test", os.Getenv("GOPATH"))
+	binDir := fmt.Sprintf("%sbin/cpp/test", os.Getenv("GOPATH"))
 	_, err = os.Stat(binDir)
 	if os.IsNotExist(err) {
 		t.Fatal("C++ binary not in ", binDir)
-	}
-}
-
-func TestCS(t *testing.T) {
-	err := cmd("go", "install", "github.com/theodus/go-transpiler")
-	if err != nil {
-		t.Fatal(err)
-	}
-	err = cmd("go-transpiler", "cs", "github.com/theodus/go-transpiler/test")
-	if err != nil {
-		t.Fatal(err)
-	}
-	binDir := fmt.Sprintf("%s/bin/cs/test.csproj", os.Getenv("GOPATH"))
-	_, err = os.Stat(binDir)
-	if os.IsNotExist(err) {
-		t.Fatal("C# file not in ", binDir)
 	}
 }
 
@@ -63,7 +47,7 @@ func TestJS(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	binDir := fmt.Sprintf("%s/bin/js/test.js", os.Getenv("GOPATH"))
+	binDir := fmt.Sprintf("%sbin/js/test.js", os.Getenv("GOPATH"))
 	_, err = os.Stat(binDir)
 	if os.IsNotExist(err) {
 		t.Fatal("JS file not in ", binDir)
