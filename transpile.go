6e6c3fd34dc73985e1ee09ac122cb58cda15c276
theodus <theodusbutler@gmail.com>
2015-11-02 10:40:43 -0500

    remove cs compile option

--- transpile.go
+++ transpile.go
@@ -13,8 +13,8 @@ import (
 func main() {
 	app := cli.NewApp()
 	app.Name = "go-transpiler"
-	app.Usage = "compile Go to Java, C++, C#, or JS using tardisgo/gopherjs"
-	app.Version = "0.2.0"
+	app.Usage = "compile Go to Java, C++, or JS using tardisgo/gopherjs"
+	app.Version = "0.2.1"
 	app.Commands = []cli.Command{
 		{
 			Name:  "java",
@@ -51,24 +51,6 @@ func main() {
 					return
 				}
 				tardis("cpp", ctx.Args()[0], "cpp")
-			},
-		}, {
-			Name:  "cs",
-			Usage: "Compile Go source to C# target",
-			Action: func(ctx *cli.Context) {
-				if len(ctx.Args()) < 1 {
-					fmt.Println("Please specify a package.")
-					return
-				}
-				if len(ctx.Args()) > 1 {
-					fmt.Println("Too many arguments!")
-					return
-				}
-				if err := cmd("go", "get", "-u", "-v", "github.com/tardisgo/tardisgo"); err != nil {
-					fmt.Println(err)
-					return
-				}
-				tardis("cs", ctx.Args()[0], "cs")
 			},
 		}, {
 			Name:  "js",
@@ -122,12 +104,6 @@ func tardis(lang, pkg, suf string) {
 		}
 	case "cpp":
 		err = cmd("cp", fmt.Sprintf("tardis/%s/Go", lang), fmt.Sprintf("%s/%s", binDir, end))
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-	case "cs":
-		err = cmd("cp", fmt.Sprintf("tardis/%s/Go.csproj", lang), fmt.Sprintf("%s/%s.csproj", binDir, end))
 		if err != nil {
 			fmt.Println(err)
 			return
