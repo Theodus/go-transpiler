7d030d115052c64f28b6a689feaeaed8db9ed960
Theodus <theodusbutler@gmail.com>
2015-11-07 11:29:58 -0500

    seperate cli from app

---
+++ cli.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/codegangsta/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "go-transpiler"
+	app.Author = "Theodore Butler"
+	app.Usage = "compile Go to Java, C++, or JS using tardisgo/gopherjs"
+	app.Version = "0.2.4"
+	app.Commands = []cli.Command{
+		{
+			Name:  "java",
+			Usage: "Compile Go source to Java target",
+			Action: func(ctx *cli.Context) {
+				if len(ctx.Args()) < 1 {
+					fmt.Println("Please specify a package.")
+					return
+				}
+				if len(ctx.Args()) > 1 {
+					fmt.Println("Too many arguments!")
+					return
+				}
+				tardis("java", ctx.Args()[0])
+			},
+		}, {
+			Name:  "cpp",
+			Usage: "Compile Go source to C++ target",
+			Action: func(ctx *cli.Context) {
+				if len(ctx.Args()) < 1 {
+					fmt.Println("Please specify a package.")
+					return
+				}
+				if len(ctx.Args()) > 1 {
+					fmt.Println("Too many arguments!")
+					return
+				}
+				tardis("cpp", ctx.Args()[0])
+			},
+		}, {
+			Name:  "js",
+			Usage: "Compile Go source to JS target",
+			Action: func(ctx *cli.Context) {
+				if len(ctx.Args()) < 1 {
+					fmt.Println("Please specify a package.")
+					return
+				}
+				if len(ctx.Args()) > 1 {
+					fmt.Println("Too many arguments!")
+					return
+				}
+				gopherjs(ctx.Args()[0])
+			},
+		},
+	}
+	app.Run(os.Args)
+}
